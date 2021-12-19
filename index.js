eaa0f0196cea0ed2a8e776e53b38a39ef02292c3
alirezacOd <gamer.alireza86@gmail.com>
2021-12-19 17:43:08 +0330

    alireza

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ import router from './router/router.js'
 import mongoose from 'mongoose'
 
 const app = express()
-
 app.use(express.json({ limit: '10mb', extended: true }))
-app.use(express.urlencoded({ limit: '10mb', extended: true }))
+app.use(express.urlencoded({ limit: '', extended: true }))
 app.use(router)
 app.listen(3000, () => {
     console.log('server is started on port 3000')
 })
-id              
-    
